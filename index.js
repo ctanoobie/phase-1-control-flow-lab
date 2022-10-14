a0c285fb50f32c492684544ccb44170dd67a9953
Christine Tanabe <tanoobie@icloud.com>
2022-10-13 18:14:52 -0700

    commit tree 6e9e12be19e4a4b43ae1fadc0279ac01df3bcc2f

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+
+let result
+
+if (someValue <= 400) {
+  result = "This one is on me!";
+
+} else if (someValue > 400 && someValue < 2000) {
+  result = "That will be twenty bucks.";
+
+} else if (someValue > 2000 && someValue < 2500) {
+  result = "I will gladly take your thirty bucks.";
+
+} else if (someValue > 2500) {
+  result = "No can do.";
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+return result
+}
+
+
+
+
+
+function ternaryCheckCity(someCity){
+const message = (someCity = NYC) ? "Ok, sounds good." : "No go.";
+
+return message;
 }
 
 function switchOnCharmFromTip(){
